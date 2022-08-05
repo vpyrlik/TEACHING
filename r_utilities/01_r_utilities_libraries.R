97599cee9eb9126f6d8ab10d76a3bd7df1e49eee
Vladimir Pyrlik <vladimir.pyrlik@gmail.com>
2022-08-05 18:44:05 +0200

    1: basic packages added; rmd templates added

---
+++ r_utilities/01_r_utilities_libraries.R
@@ -0,0 +1,17 @@
+# reports, slides, documentation -----
+library(rmarkdown)
+library(knitr)
+library(xaringan)
+library(stargazer)
+
+# data manipulation -----
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+# statistics and econometrics -----
+library(lmtest)
+library(sandwich)
+
+# plots -----
+library(ggplot2)
